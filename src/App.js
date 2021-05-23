b77d7e0d4f86e9090a5e2d1a65829c5d763cf598
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-05-23 14:54:07 +0300

    creat-function-requestFromAPI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+class App extends React.Component {
+   
+  constructor(props){
+    super(props);
+    this.state = {
+
+    
+  }
+}
+//here i will use a async in order to wait the complier to complete all senetnse of code to do'nt make undefind in console 
+//This function will do the request from web and stor the data in varible in order that is response
+
+getLoction =async(e)=>{
+  e.preventDefault();
+
+
+  let locUrl= ''
+
+
+}
+   render(){
+     return(
+       <>
+       </>
+     )
+   }
 }
 
 export default App;
